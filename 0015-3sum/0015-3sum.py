a7ca818922b3e24444bdf2efda3b1c4535232aa6
MansurOP <102641315+MansurOP@users.noreply.github.com>
2024-09-01 14:24:39 +0530

    Time: 699 ms (57.98%), Space: 20.7 MB (31.52%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,30 +1,28 @@
-from typing import List
-
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        nums.sort()  # Sorting the array first
         res = []
+        nums.sort()
 
         for i in range(len(nums)):
-            # To avoid duplicates, skip the same value
-            if i > 0 and nums[i] == nums[i - 1]:
+            if i>0 and nums[i] == nums[i-1]:
                 continue
+                    
+            j , k = i+1 , len(nums)-1
 
-            left, right = i + 1, len(nums) - 1
-
-            while left < right:
-                total = nums[i] + nums[left] + nums[right]
-                if total == 0:
-                    res.append([nums[i], nums[left], nums[right]])
-                    while left < right and nums[left] == nums[left + 1]:
-                        left += 1  # Skip duplicates
-                    while left < right and nums[right] == nums[right - 1]:
-                        right -= 1  # Skip duplicates
-                    left += 1
-                    right -= 1
-                elif total < 0:
-                    left += 1
+                    
+            while j < k :
+                t = nums[i]+nums[j]+nums[k]
+                if t > 0:
+                    k-=1
+                elif t<0:
+                    j+=1
                 else:
-                    right -= 1
+                    res.append([nums[i],nums[j],nums[k]])
+                    j+=1
 
+                    while nums[j]==nums[j-1] and j<k:
+                        j+=1
         return res
+
+                    
+                        
